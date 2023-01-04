8043bdbfd25add4ae404349e23f5b5479733f302
JUHYEON LEE <godseoril812@gmail.com>
2023-01-04 17:13:50 +0900

    hello world

---
+++ variables/Hello World.dart
@@ -0,0 +1,7 @@
+// main함수는 모든 Dart 프로그램의 Entry point이다.
+// main 함수에서 쓴 코드가 호출된다. (만약 main이 없다면 실행이 되지 않음)
+// dart는 자동으로 세미콜론을 붙여주지 않기 때문에 직접 붙여야 한다. (일부러 세미콜론을 안 쓸 때가 있기 때문)
+
+void main() {
+  print("hello world");
+}
